ec560a7b8235ac393810f9f92365a139e517a175
egar-learner <75212187+egar-learner@users.noreply.github.com>
2021-03-09 13:30:02 +0530

    Changes for the fix to get the thing running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+
+declare const annyang: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Developers';
+	voiceActiveSectionDisabled: boolean = true;
+	voiceActiveSectionError: boolean = false;
+	voiceActiveSectionSuccess: boolean = false;
+	voiceActiveSectionListening: boolean = false;
+	voiceText: any;
+
+  constructor(private ngZone: NgZone){}
+
+	initializeVoiceRecognitionCallback(): void {
+		annyang.addCallback('error', (err: { error: string; }) => {
+      if(err.error === 'network'){
+        this.voiceText = "Internet is require";
+        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      } else if (this.voiceText === undefined) {
+				this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+      console.log(this.voiceText);
+		});
+
+		annyang.addCallback('soundstart', (res: any) => {
+      this.ngZone.run(() => this.voiceActiveSectionListening = true);
+      console.log(this.voiceText);
+		});
+
+		annyang.addCallback('end', () => {
+      if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+      console.log(this.voiceText);
+		});
+
+		annyang.addCallback('result', (userSaid: any[]) => {
+			this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+			let queryText: any = userSaid[0];
+
+			annyang.abort();
+
+      this.voiceText = queryText;
+
+			this.ngZone.run(() => this.voiceActiveSectionListening = false);
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      console.log(this.voiceText);
+		});
+	}
+
+	startVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = false;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+    this.voiceText = undefined;
+
+		if (annyang) {
+			let commands = {
+				'demo-annyang': () => { }
+			};
+
+			annyang.addCommands(commands);
+
+      this.initializeVoiceRecognitionCallback();
+
+			annyang.start({ autoRestart: false });
+      console.log(this.voiceText);
+		}
+	}
+
+	closeVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = true;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+		this.voiceActiveSectionListening = false;
+		this.voiceText = undefined;
+
+		if(annyang){
+      annyang.abort();
+      console.log(this.voiceText);
+    }
+	}
 }
